e96e40407139e197106d59f260f65c471dc2579f
Guido Giuntoli <gagiuntoli@gmail.com>
2025-05-07 10:51:18 +0200

    add triangle3, quadrilateral4, hexagon8

--- src/fem.h
+++ src/fem.h
@@ -37,15 +37,15 @@ typedef std::array<std::array<std::array<double, MAX_NGP>, 3>, MAX_SHAPES> Deriv
 
 class Shape {
  public:
-  constexpr Shape(const std::array<Node, MAX_NGP>& gps_, const std::array<double, MAX_NGP>& wgs_, size_t ngp_,
-                  Shapes sh_, Derivatives dsh_, size_t nsh_)
-      : gps(gps_), wgs(wgs_), ngp(ngp_), sh(sh_), dsh(dsh_), nsh(nsh_) {}
+  constexpr Shape(size_t ngp_, const std::array<Node, MAX_NGP>& gps_, const std::array<double, MAX_NGP>& wgs_,
+                  size_t nsh_, Shapes sh_, Derivatives dsh_)
+      : ngp(ngp_), gps(gps_), wgs(wgs_), nsh(nsh_), sh(sh_), dsh(dsh_) {}
+  const size_t ngp;
   const std::array<Node, MAX_NGP> gps;
   const std::array<double, MAX_NGP> wgs;
-  const size_t ngp;
+  const size_t nsh;
   const Shapes sh;
   const Derivatives dsh;
-  const size_t nsh;
 
   std::string toString() const {
     std::ostringstream oss;
@@ -85,7 +85,7 @@ class Shape {
 
 class Segment2 : public Shape {
  public:
-  constexpr Segment2() : Shape(makeGPS(), makeWGS(), 2, makeShapes(), makeDerivatives(), 2) {}
+  constexpr Segment2() : Shape(2, makeGPS(), makeWGS(), 2, makeShapes(), makeDerivatives()) {}
 
   std::string toString() const {
     std::ostringstream oss;
@@ -123,6 +123,212 @@ class Segment2 : public Shape {
     for (size_t gp = 0; gp < 2; gp++) {
       ds[0][0][gp] = -0.5;
       ds[1][0][gp] = +0.5;
+    }
+    return ds;
+  }
+};
+
+class Triangle3 : public Shape {
+ public:
+  constexpr Triangle3() : Shape(3, makeGPS(), makeWGS(), 3, makeShapes(), makeDerivatives()) {}
+
+  std::string toString() const {
+    std::ostringstream oss;
+    oss << "Triangle Linear" << std::endl;
+    oss << Shape::toString();
+    return oss.str();
+  }
+
+ private:
+  constexpr std::array<Node, MAX_NGP> makeGPS() {
+    std::array<Node, MAX_NGP> arr{};
+    arr[0] = {+0.166666667, +0.166666667, 0};
+    arr[1] = {+0.666666667, +0.166666667, 0};
+    arr[2] = {+0.166666667, +0.666666667, 0};
+    return arr;
+  }
+
+  constexpr std::array<double, MAX_NGP> makeWGS() {
+    std::array<double, MAX_NGP> arr{};
+    arr[0] = 0.166666667;
+    arr[1] = 0.166666667;
+    arr[2] = 0.166666667;
+    return arr;
+  }
+
+  constexpr Shapes makeShapes() {
+    Shapes sh{};
+    for (size_t gp = 0; gp < 3; gp++) {
+      sh[0][gp] = 1.0 - gps[gp].x - gps[gp].y;
+      sh[1][gp] = gps[gp].x;
+      sh[2][gp] = gps[gp].y;
+    }
+    return sh;
+  }
+
+  constexpr Derivatives makeDerivatives() {
+    Derivatives ds{};
+    for (size_t gp = 0; gp < 3; gp++) {
+      ds[0][0][gp] = -1.0;
+      ds[0][1][gp] = -1.0;
+
+      ds[1][0][gp] = +1.0;
+      ds[1][1][gp] = +0.0;
+
+      ds[2][0][gp] = +0.0;
+      ds[2][1][gp] = +1.0;
+    }
+    return ds;
+  }
+};
+
+class Quadrilateral4 : public Shape {
+ public:
+  constexpr Quadrilateral4() : Shape(4, makeGPS(), makeWGS(), 4, makeShapes(), makeDerivatives()) {}
+
+  std::string toString() const {
+    std::ostringstream oss;
+    oss << "Quad Linear" << std::endl;
+    oss << Shape::toString();
+    return oss.str();
+  }
+
+ private:
+  constexpr std::array<Node, MAX_NGP> makeGPS() {
+    std::array<Node, MAX_NGP> arr{};
+    arr[0] = {-0.577350269189626, -0.577350269189626, 0};
+    arr[1] = {-0.577350269189626, +0.577350269189626, 0};
+    arr[2] = {+0.577350269189626, -0.577350269189626, 0};
+    arr[3] = {+0.577350269189626, +0.577350269189626, 0};
+    return arr;
+  }
+
+  constexpr std::array<double, MAX_NGP> makeWGS() {
+    std::array<double, MAX_NGP> arr{};
+    arr[0] = 1.0;
+    arr[1] = 1.0;
+    arr[2] = 1.0;
+    arr[3] = 1.0;
+    return arr;
+  }
+
+  constexpr Shapes makeShapes() {
+    Shapes sh{};
+    for (size_t gp = 0; gp < 4; gp++) {
+      sh[0][gp] = (1.0 - gps[gp].x) * (1.0 - gps[gp].y) * 0.25;
+      sh[1][gp] = (1.0 - gps[gp].x) * (1.0 + gps[gp].y) * 0.25;
+      sh[2][gp] = (1.0 + gps[gp].x) * (1.0 - gps[gp].y) * 0.25;
+      sh[3][gp] = (1.0 + gps[gp].x) * (1.0 + gps[gp].y) * 0.25;
+    }
+    return sh;
+  }
+
+  constexpr Derivatives makeDerivatives() {
+    Derivatives ds{};
+    for (size_t gp = 0; gp < 4; gp++) {
+      ds[0][0][gp] = -1.0 * (1.0 - gps[gp].y) * 0.25;
+      ds[0][1][gp] = -1.0 * (1.0 - gps[gp].x) * 0.25;
+
+      ds[1][0][gp] = -1.0 * (1.0 + gps[gp].y) * 0.25;
+      ds[1][1][gp] = +1.0 * (1.0 - gps[gp].x) * 0.25;
+
+      ds[2][0][gp] = +1.0 * (1.0 - gps[gp].y) * 0.25;
+      ds[2][1][gp] = -1.0 * (1.0 + gps[gp].x) * 0.25;
+
+      ds[3][0][gp] = +1.0 * (1.0 + gps[gp].y) * 0.25;
+      ds[3][1][gp] = +1.0 * (1.0 + gps[gp].x) * 0.25;
+    }
+    return ds;
+  }
+};
+
+class Hexagon8 : public Shape {
+ public:
+  constexpr Hexagon8() : Shape(8, makeGPS(), makeWGS(), 8, makeShapes(), makeDerivatives()) {}
+
+  std::string toString() const {
+    std::ostringstream oss;
+    oss << "Hexagon Linear" << std::endl;
+    oss << Shape::toString();
+    return oss.str();
+  }
+
+ private:
+  constexpr std::array<Node, MAX_NGP> makeGPS() {
+    std::array<Node, MAX_NGP> arr{};
+    arr[0] = {-0.577350269189626, -0.577350269189626, -0.577350269189626};
+    arr[1] = {-0.577350269189626, +0.577350269189626, -0.577350269189626};
+    arr[2] = {+0.577350269189626, -0.577350269189626, -0.577350269189626};
+    arr[3] = {+0.577350269189626, +0.577350269189626, -0.577350269189626};
+    arr[4] = {-0.577350269189626, -0.577350269189626, +0.577350269189626};
+    arr[5] = {-0.577350269189626, +0.577350269189626, +0.577350269189626};
+    arr[6] = {+0.577350269189626, -0.577350269189626, +0.577350269189626};
+    arr[7] = {+0.577350269189626, +0.577350269189626, +0.577350269189626};
+    return arr;
+  }
+
+  constexpr std::array<double, MAX_NGP> makeWGS() {
+    std::array<double, MAX_NGP> arr{};
+    arr[0] = 1.0;
+    arr[1] = 1.0;
+    arr[2] = 1.0;
+    arr[3] = 1.0;
+    arr[4] = 1.0;
+    arr[5] = 1.0;
+    arr[6] = 1.0;
+    arr[7] = 1.0;
+    return arr;
+  }
+
+  constexpr Shapes makeShapes() {
+    Shapes sh{};
+    for (size_t gp = 0; gp < 8; gp++) {
+      sh[0][gp] = (1.0 - gps[gp].x) * (1.0 - gps[gp].y) * (1.0 - gps[gp].z) * 0.125;
+      sh[1][gp] = (1.0 - gps[gp].x) * (1.0 + gps[gp].y) * (1.0 - gps[gp].z) * 0.125;
+      sh[2][gp] = (1.0 + gps[gp].x) * (1.0 - gps[gp].y) * (1.0 - gps[gp].z) * 0.125;
+      sh[3][gp] = (1.0 + gps[gp].x) * (1.0 + gps[gp].y) * (1.0 - gps[gp].z) * 0.125;
+      sh[4][gp] = (1.0 - gps[gp].x) * (1.0 - gps[gp].y) * (1.0 + gps[gp].z) * 0.125;
+      sh[5][gp] = (1.0 - gps[gp].x) * (1.0 + gps[gp].y) * (1.0 + gps[gp].z) * 0.125;
+      sh[6][gp] = (1.0 + gps[gp].x) * (1.0 - gps[gp].y) * (1.0 + gps[gp].z) * 0.125;
+      sh[7][gp] = (1.0 + gps[gp].x) * (1.0 + gps[gp].y) * (1.0 + gps[gp].z) * 0.125;
+    }
+    return sh;
+  }
+
+  constexpr Derivatives makeDerivatives() {
+    Derivatives ds{};
+    for (size_t gp = 0; gp < 8; gp++) {
+      ds[0][0][gp] = -1.0 * (1.0 - gps[gp].y) * (1.0 - gps[gp].z) * 0.125;
+      ds[0][1][gp] = (1.0 - gps[gp].x) * -1.0 * (1.0 - gps[gp].z) * 0.125;
+      ds[0][2][gp] = (1.0 - gps[gp].x) * (1.0 - gps[gp].y) * -1.0 * 0.125;
+
+      ds[1][0][gp] = -1.0 * (1.0 + gps[gp].y) * (1.0 - gps[gp].z) * 0.125;
+      ds[1][1][gp] = (1.0 - gps[gp].x) * +1.0 * (1.0 - gps[gp].z) * 0.125;
+      ds[1][2][gp] = (1.0 - gps[gp].x) * (1.0 + gps[gp].y) * -1.0 * 0.125;
+
+      ds[2][0][gp] = +1.0 * (1.0 - gps[gp].y) * (1.0 - gps[gp].z) * 0.125;
+      ds[2][1][gp] = (1.0 + gps[gp].x) * -1.0 * (1.0 - gps[gp].z) * 0.125;
+      ds[2][2][gp] = (1.0 + gps[gp].x) * (1.0 - gps[gp].y) * -1.0 * 0.125;
+
+      ds[3][0][gp] = +1.0 * (1.0 + gps[gp].y) * (1.0 - gps[gp].z) * 0.125;
+      ds[3][1][gp] = (1.0 + gps[gp].x) * +1.0 * (1.0 - gps[gp].z) * 0.125;
+      ds[3][2][gp] = (1.0 + gps[gp].x) * (1.0 + gps[gp].y) * -1.0 * 0.125;
+
+      ds[4][0][gp] = -1.0 * (1.0 - gps[gp].y) * (1.0 + gps[gp].z) * 0.125;
+      ds[4][1][gp] = (1.0 - gps[gp].x) * -1.0 * (1.0 + gps[gp].z) * 0.125;
+      ds[4][2][gp] = (1.0 - gps[gp].x) * (1.0 - gps[gp].y) * +1.0 * 0.125;
+
+      ds[5][0][gp] = -1.0 * (1.0 + gps[gp].y) * (1.0 + gps[gp].z) * 0.125;
+      ds[5][1][gp] = (1.0 - gps[gp].x) * +1.0 * (1.0 + gps[gp].z) * 0.125;
+      ds[5][2][gp] = (1.0 - gps[gp].x) * (1.0 + gps[gp].y) * +1.0 * 0.125;
+
+      ds[6][0][gp] = +1.0 * (1.0 - gps[gp].y) * (1.0 + gps[gp].z) * 0.125;
+      ds[6][1][gp] = (1.0 + gps[gp].x) * -1.0 * (1.0 + gps[gp].z) * 0.125;
+      ds[6][2][gp] = (1.0 + gps[gp].x) * (1.0 - gps[gp].y) * +1.0 * 0.125;
+
+      ds[7][0][gp] = +1.0 * (1.0 + gps[gp].y) * (1.0 + gps[gp].z) * 0.125;
+      ds[7][1][gp] = (1.0 + gps[gp].x) * +1.0 * (1.0 + gps[gp].z) * 0.125;
+      ds[7][2][gp] = (1.0 + gps[gp].x) * (1.0 + gps[gp].y) * +1.0 * 0.125;
     }
     return ds;
   }
